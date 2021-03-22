ce90bbbc2e11df8e1545dcb2455d6b852d17b0e3
Khushee Jain <jain.khushee05@gmail.com>
2021-03-22 12:41:18 +0530

    Final Commit

--- BSTNode.java
+++ BSTNode.java
@@ -2,8 +2,12 @@ package BST;
 
 public class BSTNode<T> {
 	
-	T data;
+	T data;							//Common node to create BST
 	BSTNode<T> Left;
 	BSTNode<T> Right;
 	
+	BSTNode( T data)
+	{
+		this.data = data;
+	}
 }
--- printBST.java
+++ printBST.java
@@ -1,9 +1,45 @@
 package BST;
 
 public class printBST {
+	
+	public static void printBinaryTree(BSTNode<Integer> root)
+	{
+		if(root == null)
+			return;
+		System.out.println("Root data is: " + root.data);
+		
+		if(root.Left != null)
+		{
+			System.out.println("Left data: " + root.Left.data);
+			
+		}
+		else
+			System.out.println("No Left Child");
+		
+		if(root.Right!=null)
+		{
+			System.out.println("Right data: " + root.Right.data);
+			
+		}
+		else
+			System.out.println("No Right Child");
+		
+		System.out.println();
+		printBinaryTree(root.Left);
+		printBinaryTree(root.Right);
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			
+		BSTNode<Integer> root = new BSTNode<Integer>(3);
+		BSTNode<Integer> leftroot = new BSTNode<Integer>(2);
+		BSTNode<Integer> rightroot = new BSTNode<Integer>(4);
+		
+		root.Left = leftroot;
+		root.Right = rightroot;
+		
+		printBinaryTree(root);
 
 	}
 
